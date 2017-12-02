2be299504cd5a758b90617ad04aac439df2f4c87
nve3pd <uchihara1229@gmail.com>
2017-12-02 16:58:56 +0900

    initial commit

---
+++ src/rhyme.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env/python3
+
+import os
+import sys
+import marisa_trie
+from pykakasi import kakasi
+
+kakasi_py = kakasi()
+kakasi_py.setMode('H', 'a')
+kakasi_py.setMode('K', 'a')
+kakasi_py.setMode('J', 'a')
+conv = kakasi_py.getConverter()
+
+
+def _roman_str(text):
+    """ 与えられた文字列をローマ字に変換して返す """
+    return conv.do(text)
+
+
+def _vowel_str(word):
+    """ 母音のみの文字列にして返す """
+    vowel_str = ""
+    for i in _roman_str(word):
+        if i in "aiueon-":
+            vowel_str += i
+
+    return vowel_str
+
+
+def _make_rhyme_words(w):
+    """ 韻を踏んでいそうな単語を探索してリストで返す """
+    files = os.listdir("./src/dictionary/")
+    vowel_word = _vowel_str(w)
+    vowel_length = len(vowel_word)
+    rhyme_words = []
+
+    for i in files:
+        with open("./src/dictionary/" + i, "r") as f:
+            trie = marisa_trie.Trie(list(f))
+            res = trie.keys(vowel_word)
+#            print(res)
+
+            for j in res:
+                tmp = j.split()
+                if len(tmp[0]) == vowel_length:  # 母音が全一致の場合のみ
+                    rhyme_words.append(tmp[1])
+
+    return rhyme_words
+
+
+def rhymes(text):
+    """ 韻を踏んでいそうな単語をリストで返す """
+    roman = _roman_str(text)
+    print(roman)
+    return _make_rhyme_words(roman)
+
+
+if __name__ == "__main__":
+    res = rhyme(sys.argv[1] if len(sys.argv) >= 2 else "")
+    print(res)
